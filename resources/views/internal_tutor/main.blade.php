9eda7bbc73d0515f79383e2ce1826b93691c218c
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2023-05-31 14:28:57 +0700

    test

--- resources/views/internal_tutor/main.blade.php
+++ resources/views/internal_tutor/main.blade.php
@@ -1,26 +1,29 @@
-<!DOCTYPE html>
-<html>
 <head>
-    <!-- Include your head content here -->
-    <title>My Website</title>
+    <!-- Bootstrap -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href=”css/bootstrap.min.css” rel=”stylesheet”>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <!-- Include your header content here -->
+
+    @section ('header')
+        @if (auth()->check())
+            @include('internal_tutor.includes.header')
+        @endif
+    @endsection
+
+    @yield('alerts.main')
     @yield('header')
-
-    <!-- Content from other views will be included here -->
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="{{ url('styles/font.css') }}" rel="stylesheet" type = "text/css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/app.css') }}">
+        <meta name="google-signin-client_id"
+         content="942875105859-fh3uu63rjvn0gjrkp9m8gdabb583nq45.apps.googleusercontent.com">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
     @yield('content')
-
-    <!-- Include your scripts here -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <script src="{{ asset('resources/js/app.js') }}"></script>
+    
     <script>
         toastr.options = {
             "closeButton": true,
@@ -54,6 +57,10 @@
         @if(Session::has('error'))
             toastr.error("{{ Session::get('error') }}")
         @endif
+
+        
     </script>
-</body>
-</html>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="{{ asset('resources/js/app.js') }}"></script>
+
+</head>

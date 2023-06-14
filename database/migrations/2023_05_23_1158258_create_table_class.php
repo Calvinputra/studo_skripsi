aad3e350e05689e47776ff7b7414b238eeff1432
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2023-06-14 17:45:03 +0700

    test

--- database/migrations/2023_05_23_1158258_create_table_class.php
+++ database/migrations/2023_05_23_1158258_create_table_class.php
@@ -22,7 +22,6 @@ class CreateTableClass extends Migration
             $table->string('slug');
             $table->string('thumbnail');
             $table->string('category');
-            $table->string('duration');
             $table->integer('price');
             $table->integer('discount');
             $table->enum('status', ['active', 'deactive'])->default('active');

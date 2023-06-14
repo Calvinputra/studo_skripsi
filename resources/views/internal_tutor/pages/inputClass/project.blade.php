8acb93d024a1c27389f21ccc6ee121edc4983d4d
Jason Renata <54830225+jasonrenata@users.noreply.github.com>
2023-06-14 17:21:30 +0700

    test

--- resources/views/internal_tutor/pages/inputClass/project.blade.php
+++ resources/views/internal_tutor/pages/inputClass/project.blade.php
@@ -37,7 +37,7 @@
                                 <div class="d-flex align-items-center">
                                     <div class="btn-info-kelas" style="margin:40 0px;">
                                         <p class="m-0" style="color:white;">
-                                            informasi Umum
+                                            Informasi Umum
                                         </p>
                                     </div>
                                     <div style="margin:0px 16px;">
@@ -48,6 +48,16 @@
                                     <div class="btn-info-kelas" style="margin:40 0px;">
                                         <p class="m-0" style="">
                                             Materi Pembelajaran
+                                        </p>
+                                    </div>
+                                    <div style="margin:0px 16px;">
+                                        <svg width="42" height="23" viewBox="0 0 42 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M41.0607 12.5607C41.6464 11.9749 41.6464 11.0251 41.0607 10.4393L31.5147 0.893398C30.9289 0.307611 29.9792 0.307611 29.3934 0.893398C28.8076 1.47919 28.8076 2.42893 29.3934 3.01472L37.8787 11.5L29.3934 19.9853C28.8076 20.5711 28.8076 21.5208 29.3934 22.1066C29.9792 22.6924 30.9289 22.6924 31.5147 22.1066L41.0607 12.5607ZM0 13H40V10H0V13Z" fill="#063852"/>
+                                        </svg>
+                                    </div>
+                                    <div class="btn-info-kelas" style="margin:40 0px;">
+                                        <p class="m-0" style="">
+                                            Quest
                                         </p>
                                     </div>
                                     <div style="margin:0px 16px;">

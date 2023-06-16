c79cd2521757c57a3bc8c5cf134c2d3ca91db953
Jason Renata <54830225+jasonrenata@users.noreply.github.com>
2023-06-16 22:54:22 +0700

    sign up done

--- resources/views/internal_tutor/pages/auth/signup.blade.php
+++ resources/views/internal_tutor/pages/auth/signup.blade.php
@@ -1,15 +1,24 @@
 @extends('internal_tutor.main')
 <style>
-    body{margin: 0;padding: 0;background: url(https://i.ibb.co/VQmtgjh/6845078.png) no-repeat;height: 100vh;font-family: sans-serif;background-size: cover;background-repeat: no-repeat;background-position: center;overflow: hidden}@media screen and (max-width: 600px;){body{background-size: cover;: fixed}}#particles-js{height: 100%}.loginBox{position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);width: 350px;min-height: 200px;background: #000000;border-radius: 10px;padding: 40px;box-sizing: border-box}.user{margin: 0 auto;display: block;margin-bottom: 20px}h3{margin: 0;padding: 0 0 20px;color: #59238F;text-align: center}.loginBox input{width: 100%;margin-bottom: 20px}.loginBox input[type="text"], .loginBox input[type="password"]{border: none;border-bottom: 2px solid #262626;outline: none;height: 40px;color: #fff;background: transparent;font-size: 16px;padding-left: 20px;box-sizing: border-box}.loginBox input[type="text"]:hover, .loginBox input[type="password"]:hover{color: #42F3FA;border: 1px solid #42F3FA;box-shadow: 0 0 5px rgba(0,255,0,.3), 0 0 10px rgba(0,255,0,.2), 0 0 15px rgba(0,255,0,.1), 0 2px 0 black}.loginBox input[type="text"]:focus, .loginBox input[type="password"]:focus{border-bottom: 2px solid #42F3FA}.inputBox{position: relative}.inputBox span{position: absolute;top: 10px;color: #262626}.loginBox input[type="submit"]{border: none;outline: none;height: 40px;font-size: 16px;background: #59238F;color: #fff;border-radius: 20px;cursor: pointer}.loginBox a{color: #262626;font-size: 14px;font-weight: bold;text-decoration: none;text-align: center;display: block}a:hover{color: #00ffff}p{color: #0000ff}
+    body{margin: 0;padding: 0;background-color: rgba(103, 109, 111, 0.3) !important;height: 100vh;font-family: sans-serif;background-size: cover;background-repeat: no-repeat;background-position: center;overflow: hidden}
+    @media screen and (max-width: 600px;){body{background-size: cover;: fixed}}#particles-js{height: 100%}
+    
+    .loginBox{position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);width: 350px;min-height: 200px;background:white;;border-radius: 10px;padding: 40px;box-sizing: border-box}.user{margin: 0 auto;display: block;margin-bottom: 0px}h3{margin: 0;padding: 0 0 20px;color: black;text-align: center}.loginBox input{width: 100%;margin-bottom: 0px}.loginBox input[type="text"], .loginBox input[type="password"]{border: none;border-bottom: 2px solid #262626;outline: none;height: 40px;color: #fff;background: transparent;font-size: 16px;padding-left: 20px;box-sizing: border-box}.loginBox input[type="text"]:hover, .loginBox input[type="password"]:hover{color: black;border: 1px solid black;box-shadow: 0 0 5px rgba(0,255,0,.3), 0 0 10px rgba(0,255,0,.2), 0 0 15px rgba(0,255,0,.1), 0 2px 0 black}.loginBox input[type="text"]:focus, .loginBox input[type="password"]:focus{border-bottom: 0px solid black}.inputBox{position: relative}.inputBox span{position: absolute;top: 10px;color: #262626}.loginBox input[type="submit"]{border: none;outline: none;height: 40px;font-size: 16px;background: black;color: #fff;border-radius: 5px;cursor: pointer}.loginBox a{color: #262626;font-size: 14px;font-weight: bold;text-decoration: none;text-align: center;display: block}a:hover{color: #00ffff}p{color: #0000ff}
     .password-input {
         position: relative;
     }
-
+    .loginBox input[type="password"]{
+        color:black !important;
+        border-bottom:none;
+    }
     .password-input .password-icon {
         position: absolute;
         right: 10px;
         top: 10px;
         cursor: pointer;
+    }
+    .loginBox input[type="text"]{
+        color:black;
     }
 </style>
 
@@ -19,9 +28,9 @@
     <form action="{{ route('internal_tutor.post.signup') }}" method="post">
         @csrf
         <div class="inputBox"> 
-            <input id="name" type="name" name="name" placeholder="name"> 
-            <input id="email" type="email" name="email" placeholder="email">
-            <div class="password-input">
+            <input id="name" type="name" name="name" placeholder="name"  style="height:40px;padding:20px;"> 
+            <input id="email" type="email" name="email" placeholder="email"  style="height:40px;padding:20px;margin:10px 0px;">
+            <div class="password-input" style="color:black; border:1px solid black;margin:0px 0px 20px 0px;">
                 <input id="password" type="password" name="password" placeholder="password"> 
                 <i id="toggle-password" class="fa fa-eye password-icon"></i>
             </div>
@@ -30,8 +39,8 @@
     </form> 
 
     <div class="text-center">
-        <p style="color:#fff">Sudah punya akun?</p>
-        <a href="{{route('internal_tutor.signin')}}" style="color: #59238F;font-size:18px">Sign-In</a>
+        <p style="color:black">Sudah punya akun?</p>
+        <a href="{{route('internal_tutor.signin')}}" style="color: black;font-size:18px">Sign-In</a>
     </div>
 </div>
 <script>

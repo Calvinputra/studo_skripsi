9aa8ffb1f05a739e374582227669b0d4959c6be8
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2023-06-19 16:08:41 +0700

    test

--- app/Http/Controllers/studo/SiteController.php
+++ app/Http/Controllers/studo/SiteController.php
@@ -14,8 +14,9 @@ class SiteController extends Controller
     {
     if (auth()->check()){
         $user = User::find(auth()->user()->id);
-        $subscription = Subscription::join('classes', 'classes.id', '=', 'subscription.class_id')
-        ->join('tutors', 'tutors.id', '=', 'subscription.tutor_id')
+        $subscriptions = Subscription::join('classes', 'classes.id', '=', 'subscription.class_id')
+        ->join('users', 'users.id', '=', 'subscription.user_id')
+        ->join('tutors', 'tutors.id', '=', 'classes.tutor_id')
         ->select([
             'classes.*',
             'tutors.name as tutor_name',
@@ -25,7 +26,7 @@ class SiteController extends Controller
     }
     else{
         $user = NULL;
-        $subscription = NULL;
+        $subscriptions = NULL;
     }
         $classes = Classes::join('tutors', 'tutors.id', '=', 'classes.tutor_id')
         ->select([
@@ -34,11 +35,10 @@ class SiteController extends Controller
             'tutors.email as tutor_email',
         ])->get();
 
-
         return view('studo.pages.site.index', [
             'user' => $user,
             'classes' => $classes,
-            'subscription' => $subscription
+            'subscriptions' => $subscriptions
         ]);
     }
 }

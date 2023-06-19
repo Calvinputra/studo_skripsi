c2744758f56400cebb1c7133c9413ce7fd179f53
Jason Renata <54830225+jasonrenata@users.noreply.github.com>
2023-06-19 16:16:38 +0700

    header footer done

--- resources/views/studo/includes/footer.blade.php
+++ resources/views/studo/includes/footer.blade.php
@@ -1,13 +1,13 @@
-<div style="display: flex;flex-direction: column;justify-content: center;align-items: center;padding: 40px 300px;background: #E6EBED;margin-top:40px;">
+<div style="display: flex;flex-direction: column;justify-content: center;align-items: center;padding: 40px 220px;background: #E6EBED;margin-top:40px;">
     <div>
-        <center>
-            <img style="width: 90px;height: 40px;margin-bottom:24px;" class="" src="{{ asset('/thumbnails/studo.png') }}" alt="">
+        <div class="d-flex align-items-center">
+            <img style="width: 90px;height: 40px;margin-bottom:24px;margin-right:24px;" class="" src="{{ asset('/thumbnails/studo.png') }}" alt="">
             <p class="text-materi" style="font-weight:500">
                 Studo adalah platform pendidikan berbasis e-learning yang memberikan akses mudah dan fleksibel 
                 ke berbagai kursus dan materi pembelajaran. Dengan kursus interaktif, video pelajaran, dan forum diskusi, 
                 pengguna dapat belajar secara mandiri dan berinteraksi dengan instruktur dan sesama peserta kursus.
             </p>
-        </center>
+        </div>
     </div>
 </div>
 <div class="" style="background:#063852;padding: 8px 40px;">

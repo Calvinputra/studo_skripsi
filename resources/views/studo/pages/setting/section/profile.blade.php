5eaef7ecf36a4d36b71f4dd8689db5fda20ca149
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2023-06-25 02:10:18 +0700

    test

--- resources/views/studo/pages/setting/section/profile.blade.php
+++ resources/views/studo/pages/setting/section/profile.blade.php
@@ -11,7 +11,9 @@
                 @if ($user->avatar)
                     <img style="width: 100px;height: 100px;border-radius:100px;" src="{{ $user->avatar }}" alt="">
                 @else
-                    <img style="margin-top: 40px;" src="https://via.placeholder.com/300" alt="">
+                    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="50" cy="50" r="50" fill="#D9D9D9"/>
+                    </svg>
                 @endif
             </label>
             <button class="btn my-4 my-sm-0 mx-4" style="color:white;background:rgba(6, 56, 82, 0.1);" type="button" onclick="selectImage()">
